5aeae844166ed664fd3f463e02f98688a0a74be0
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-01-26 18:39:55 +0530

    second commit

--- CPP/first.cpp
+++ CPP/first.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a;
+    int b;
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    cin>>a;
+    cin>>b;
     string s;
     getline(cin,s);
-    cout<<"a="<<a;
+    cout<<"b="<<b;
 }
