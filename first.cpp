b7f3fae6af75663679b6f315248f2d9737d1ccf5
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-01-26 19:15:07 +0530

    adding first file

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int b;
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    cin>>b;
+    string s;
+    getline(cin,s);
+    cout<<"b="<<b;
+}
