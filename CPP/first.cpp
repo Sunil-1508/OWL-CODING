ecc770dddc2a7d3d34c25e1e0ba8609d6442cd0b
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-01-26 18:35:21 +0530

    initial commit

---
+++ CPP/first.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    cin>>a;
+    string s;
+    getline(cin,s);
+    cout<<"a="<<a;
+}
